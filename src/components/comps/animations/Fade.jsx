5b7a2dfdc506e2592925c669e4adb395166bcbe2
Michael Maccio <miklmccio@gmail.com>
2017-10-27 02:47:18 +0200

    Fade

--- src/components/comps/animations/Fade.jsx
+++ src/components/comps/animations/Fade.jsx
@@ -1,6 +1,22 @@
+import React from 'react'
+import { Transition } from 'react-transition-group'
 import styled from 'styled-components'
 
-export const BarIcon = styled.div`
+export const FadeTransition = ({ children, ...props }) => (
+  <Transition
+    exit
+    appear
+    {...props}
+    timeout={{
+      enter: 150,
+      exit: 150,
+    }}
+  >
+    {status => children(status)}
+  </Transition>
+)
+
+export const FadeComp = styled.div`
   position: absolute;
   transition: all 150ms ease-in-out;
   display: flex;
@@ -8,7 +24,6 @@ export const BarIcon = styled.div`
   align-items: center;
   height: 50px;
   width: 50px;
-  cursor: pointer;
 
   transform: scale(
     ${(props) => {
@@ -22,7 +37,7 @@ export const BarIcon = styled.div`
       return 0.2
     }
     if (props.status === 'exited') {
-      return 1
+      return 0.2
     }
     return 0.2
   }}
@@ -38,10 +53,8 @@ export const BarIcon = styled.div`
       return 0
     }
     if (props.status === 'exited') {
-      return 1
+      return 0
     }
     return 0
   }};
 `
-export const RightBarIcon = styled(BarIcon)`right: 0;`
-export const LeftBarIcon = styled(BarIcon)`left: 0;`
