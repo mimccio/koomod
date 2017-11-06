4c057a56b03b621b87ed78708cd5cfaaee826fa3
Michael Maccio <miklmccio@gmail.com>
2017-11-06 20:20:30 +0100

    add icons, move SW in /src, more manifest

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
-import registerServiceWorker from './lib/registerServiceWorker'
+import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
