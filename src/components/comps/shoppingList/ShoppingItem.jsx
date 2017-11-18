2b7434e4c0c6f8f28d6971b1df6ffb8622cb385a
Michael Maccio <miklmccio@gmail.com>
2017-11-18 11:12:41 +0100

    shopping list item toggleable isDone only in state

--- src/components/comps/shoppingList/ShoppingItem.jsx
+++ src/components/comps/shoppingList/ShoppingItem.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
+// @flow
+import * as React from 'react'
 import styled from 'styled-components'
+
+import palette from '../../../style/palette'
 
 const ItemWrapper = styled.div`
   width: 100%;
   padding: 10px;
-  border-bottom: grey;
+  border-bottom: 1px solid ${palette.divider};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: ${({ isDone }) => (isDone ? palette.disabled : palette.text)};
+  font-style: ${({ isDone }) => (isDone ? 'italic' : 'normal')};
+  text-decoration: ${({ isDone }) => (isDone ? `line-through ${palette.disabled}` : 'none')};
+  cursor: pointer;
+  // transition: all 200 ms ease;
 `
 
-export default ({ ingredient }) => (
-  <ItemWrapper>
-    <p>{ingredient.name}</p>
+type Props = {
+  ingredient: {
+    name: string,
+    quantity: number,
+    nature: string
+  }
+}
 
-    <p>
-      {ingredient.quantity} {ingredient.nature}
-    </p>
-  </ItemWrapper>
-)
+type State = {
+  isDone: boolean
+}
+
+export default class ShoppingListItem extends React.Component<Props, State> {
+  state = {
+    isDone: false,
+  }
+  render() {
+    const { name, quantity, nature } = this.props.ingredient
+    return (
+      <ItemWrapper isDone={this.state.isDone} onClick={() => this.setState({ isDone: !this.state.isDone })}>
+        <p>{name}</p>
+
+        <p>
+          {quantity} {nature}
+        </p>
+      </ItemWrapper>
+    )
+  }
+}
